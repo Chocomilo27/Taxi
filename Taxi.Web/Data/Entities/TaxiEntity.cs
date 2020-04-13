4bf3387e9fada7b9733763500071c2ea9c2ce72c
IVAN CAMILO ALZATE OSPINA <ivanalzate213628@correo.itm.edu.co>
2020-04-13 16:25:23 -0500

    Create project, database and first Entity

---
+++ Taxi.Web/Data/Entities/TaxiEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taxi.Web.Data.Entities
+{
+    public class TaxiEntity
+    {
+        public int Id { get; set; }
+
+
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "The {0} field must have {1} characters.")] //1 refers to the first condition (6)
+        [Required(ErrorMessage = "The field {0} is mandatory.")] //The "0" refers to the name of field.
+        public string Plaque { get; set; }
+
+    }
+}
